8edc2a8617d5325c94b23cb4f53843572ad4b83a
feimengwang <wjb27470@163.com>
2017-01-09 20:18:44 +0800

    refine

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -4,15 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.packt.webstore.service.ProductService;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 	@Autowired
 	private ProductService service;
 
-	@RequestMapping("/products")
+	@RequestMapping
+	public ModelAndView list() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("products", service.getAllProducts());
+		modelAndView.setViewName("products");
+		return modelAndView;
+	}
+
+	@RequestMapping("/all")
 	public String listProducts(Model model) {
 
 		model.addAttribute("products", service.getAllProducts());
